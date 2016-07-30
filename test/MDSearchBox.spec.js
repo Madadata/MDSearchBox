fc59d618167183561a0273bb10761ae237a96ba8
zhenghe <zhenghe@madadata.com>
2016-07-30 13:11:05 +0800

    add lint

--- test/MDSearchBox.spec.js
+++ test/MDSearchBox.spec.js
@@ -1,14 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import React from 'react';
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
 import { shallow } from 'enzyme';
 import { createRenderer } from 'react-addons-test-utils';
-import MDSearchBox from 'src/MDSearchBox';
+import MDSearchBox from '../src/MDSearchBox';
 
 expect.extend(expectJSX);
 
 describe('MDSearchBox', () => {
   it('should work', () => {
 
-  })
+  });
 });
